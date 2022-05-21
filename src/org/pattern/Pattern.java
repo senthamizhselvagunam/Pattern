d1e17f90f42f606d4677c886e1bfd8a853f65735
senthu <abi@05>
2022-05-21 15:45:34 +0530

    done

---
+++ src/org/pattern/Pattern.java
@@ -0,0 +1,13 @@
+package org.pattern;
+
+public class Pattern {
+public static void main(String[] args) {
+	for (int i=0; i < 10; i++) {
+		
+ for (int j = 0; j <i; j++) {
+	System.out.print(j);
+}
+		System.out.println();
+	}
+}
+}
